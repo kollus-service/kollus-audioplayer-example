d47870b20cb3cc484faa6127918c77ebbda3ea2d
HyeonD <hdyang@catnoid.net>
2018-07-31 04:45:57 +0900

    add audiopalayer and wrapper controll example

--- customplayer.js
+++ customplayer.js
@@ -95,9 +95,29 @@ var get_contents = function() {
   ];
 };
 
-function isIE() {
-  var myNav = navigator.userAgent.toLowerCase();
-  return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
+function isIE () { 
+
+	 var word;
+
+	 var agent = navigator.userAgent.toLowerCase();
+
+	 // IE old version ( IE 10 or Lower )
+	 if ( navigator.appName == "Microsoft Internet Explorer" ) word = "msie ";
+
+	 // IE 11
+	 else if ( agent.search( "trident" ) > -1 ) word = "trident/.*rv:";
+
+	 // Microsoft Edge
+	 else if ( agent.search( "edge/" ) > -1 ) word = "edge/";
+
+	 // 그외, IE가 아니라면 ( If it's not IE or Edge )
+	 else return -1;
+
+	 var reg = new RegExp( word + "([0-9]{1,})(\\.{0,}[0-9]{0,1})" );
+
+	 if (  reg.exec( agent ) != null  ) return parseFloat( RegExp.$1 + RegExp.$2 );
+
+	 return -1;
 }
 //URL 생성 함수
 var get_url = function(cuid, mck) {
